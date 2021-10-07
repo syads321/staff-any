efcb55beb4201d3bae888a9384589ca55df57f75
irsyad.fadlillah <irsyad.fadlillah@eaciit.com>
2021-10-07 09:42:46 +0700

    remove unnecesary config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 require('dotenv').config()
 module.exports = {
-  reactStrictMode: true,
-  database: {
-    user: 'ASUS',
-    host: 'localhost',
-    database: 'staffany',
-    password: '123',
-    port: 5432,
-  }
+  reactStrictMode: true
 }
